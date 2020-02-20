4ced24cc0758ae340c9bdd21267cd81e0f3d85f1
Simon <ssundstrom@live.se>
2020-02-20 08:08:58 +0100

    first commit, spring boot set up

---
+++ src/main/java/com/master_thesis/ge/GeApplication.java
@@ -0,0 +1,13 @@
+package com.master_thesis.ge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/master_thesis/ge/GeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.master_thesis.ge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
