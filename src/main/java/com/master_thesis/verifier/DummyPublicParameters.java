8f40d40d3646868a29e7d1663943a513f1f3eadd
Simon <ssundstrom@live.se>
2020-02-20 17:30:28 +0100

    reading servers from coordinator

--- src/main/java/com/master_thesis/verifier/DummyPublicParameters.java
+++ src/main/java/com/master_thesis/verifier/DummyPublicParameters.java
@@ -1,20 +1,31 @@
 package com.master_thesis.verifier;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
 import org.springframework.stereotype.Component;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @Component
 public class DummyPublicParameters implements PublicParameters{
 
     @Override
+    @SneakyThrows
     public List<Integer> getServers() {
 
+        HttpRequest httpRequest = HttpRequest.newBuilder(URI.create("http://localhost:4000/api/server/list"))
+                .GET().build();
 
-
-        return Stream.of(1, 2).collect(Collectors.toList());
+        HttpResponse<String> response = HttpClient.newHttpClient().send(httpRequest, HttpResponse.BodyHandlers.ofString());
+        List<ServerData> servers = new ObjectMapper().readValue(response.body(), new TypeReference<>() {});
+        return servers.stream().map(ServerData::getServerID).collect(Collectors.toList());
     }
 
 }
---
+++ src/main/java/com/master_thesis/verifier/ServerData.java
@@ -0,0 +1,17 @@
+package com.master_thesis.verifier;
+
+import java.net.URI;
+
+public class ServerData {
+
+    private int serverID;
+
+    public int getServerID() {
+        return serverID;
+    }
+
+    public void setServerID(int serverID) {
+        this.serverID = serverID;
+    }
+
+}
