9176cfbd5dbd29f0afb9ef5ae379cd408f72aa11
Felix Jansson <FJ865990@goteborgenergi.intra>
2020-03-13 13:19:03 +0100

    added locks to prevent race conditions

--- src/main/java/com/master_thesis/verifier/VerifierController.java
+++ src/main/java/com/master_thesis/verifier/VerifierController.java
@@ -9,26 +9,38 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigInteger;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 @RestController
 public class VerifierController {
 
     private static final Logger log = (Logger) LoggerFactory.getLogger(VerifierController.class);
     private VerifierBuffer buffer;
+    private Lock bufferLock;
     private RSAThreshold verifier;
 
     @Autowired
     public VerifierController(VerifierBuffer buffer, RSAThreshold verifier) {
         this.buffer = buffer;
         this.verifier = verifier;
+        this.bufferLock = new ReentrantLock();
     }
 
     @PostMapping(value = "/api/partials")
-    public void receiveShare(@RequestBody PartialInfo partialInfo) {
-        log.info("Got {}", partialInfo);
-        buffer.put(partialInfo);
-        if (buffer.canCompute())
-            new Thread(() -> performComputations(partialInfo.getTransformatorID())).start();
+    public void receiveShare(@RequestBody PartialInfo partialInfo) throws InterruptedException {
+        boolean isUnlocked = bufferLock.tryLock(1, TimeUnit.SECONDS);
+        if (isUnlocked) {
+            try {
+                log.info("Got {}", partialInfo);
+                buffer.put(partialInfo);
+                if (buffer.canCompute())
+                    new Thread(() -> performComputations(partialInfo.getTransformatorID())).start();
+            } finally {
+                bufferLock.unlock();
+            }
+        }
     }
 
     private void performComputations(int transformatorID) {
